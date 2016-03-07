733711c5448e630635484eb93800346bcac687e7
Lukasz Zapala <zapala.lukasz@outlook.com>
2016-03-07 08:55:10 +0100

    bzvzjkvzj.kv

--- File Manager/File Manager/WinMain.cpp
+++ File Manager/File Manager/WinMain.cpp
@@ -1,6 +1,9 @@
 ﻿#include "CustomWindow.h"
 #include "resource.h"
 
+// God I hate WinAPI
+
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	SIZE sSize;
